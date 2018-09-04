72489c525544c342d4b4d4089f800ab59ffe9e1c
Lauren <llwor94@gmail.com>
2018-09-04 16:03:06 -0700

    sset up database

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const racerRoutes = require('./routes/racerRoutes');
+const server = express();
+const mw = require('./middleware');
+
+server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev'));
+
+server.use('/api/racers', racerRoutes);
+server.use(mw);
+
+server.get('/', (req, res) => {
+  res.send('It works mon');
+});
+
+const port = 3500;
+server.listen(port, function() {
+  console.log(`\n=== Web API Listening on http://localhost:${port} mon ===\n`);
+});
